e700077b2dadf54c1ffcd2696185309c46e8848e
sovath.koeun <sovath.koeun@PNC.LAN>
2020-02-13 20:32:43 +0700

    challenge02

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
-const DATE_HTML = document.querySelector('#date');
-const LIST_HTML = document.querySelector('#list');
-const INPUT_HTML = document.querySelector('#input');
+const DATE_HTML = document.querySelector("#date");
+const LIST_HTML = document.querySelector("#list");
+const INPUT_HTML = document.querySelector("#input");
 
 const CHECK_STYLE = "fa-check-circle";
 const UNCHECK_STYLE = "fa-circle-thin";
 const LINE_THOUGH_STYLE = "lineThrough";
 
 const options = {
-// TODO
   weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric"
 };
+const today = new Date();
+DATE_HTML.innerHTML = today.toLocaleDateString("en-US", options);
 
-const TODAY = new Date();
-DATE_HTML.innerHTML = TODAY.toLocaleDateString('en-US',options);
-// TODO: to display the date in HTML
+document.addEventListener("keyup", event => {
+	if(event.code === 'Enter'){
+		const TODOS = InputElement.value;
+		if(TODOS != "") {
+			console.log(TODOS);
+			clearInput();
+		}
+	}
+	
+});
 
+function clearInput() {
+      InputElement.value = "";
+}
