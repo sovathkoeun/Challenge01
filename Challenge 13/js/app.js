e2116b768c795c7da67eed26058f012c05f98695
sovath.koeun <sovath.koeun@pnc.lan>
2020-02-15 07:59:32 +0700

    updates

--- Challenge 13/js/app.js
+++ Challenge 13/js/app.js
@@ -85,14 +85,24 @@ function saveJson() {
 
 document.addEventListener("click", function(event) {
   const elementJob = event.target.attributes.job.value;
-  // TODO: get id 
-  const elementid = parseInt(event.target.id);
-  console.log(elementid);
-  // TODO: check the codition if elementJob === "complete" console something complete
-  if(elementJob === "complete"){
-    console.log("Hello");
-  // however if elementJob === "delete" console something delete
-  }else if(elementJob === "delete"){
-    console.log("data");
+  const elementId = parseInt(event.target.id);
+
+  if (elementJob == "complete") {
+    completeToDo(elementId);
+  } else if (elementJob == "delete") {
+    removeToDo(elementId);
   }
 });
+
+// create function removeToDo() 
+// paramenter is id
+function removeToDo(id) {
+      
+} 
+
+// create function completeToDo()
+// paramenter is id
+function completeToDo(id){
+
+}
+
